fc05a62550bc64c2ea7ed70c7918fce604923557
luranmanfio0701@gmail.com <luranmanfio0701@gmail.com>
2020-07-26 12:16:57 -0400

    modified Shiny to fit BEACH data
    made adjustments to have the shiny app reporesent the greater BEACH Data set.

--- example/BRACH_Recruitment_Review/app.R
+++ example/BRACH_Recruitment_Review/app.R
@@ -46,6 +46,7 @@ fields=c("test_id","redcap_event_name",
          "beach_study_complete",
          "beachphone_date",
                 # new data pulling, pulling dates of visits and BMI
+         "beachphone_age",
          "mom3t_prepreg_bmi",
          "mom2mo_todays_date",
          "mom2wk_todays_date")
@@ -57,7 +58,7 @@ records=c("BLS001A","BLS002A","BLS003A","BLS007A","BLS008A","BLS011A",
           "BLS041A","BLS043A","BLS044A","BLS045A","BLS048A","BLS049A",
           "BLS051A","BLS052A","BLS053A","BLS055A","BLS056A","BLS059A",
           "BLS063A","BLS064A","BLS065A","BLS068A","BLS073A","BLS078A",
-          "BLS079A","BLS083A")
+          "BLS079A","BLS083A","BLS006A")
 
 # Issue with redcap_read(): 
 # Error in inherits(ds, "data.frame") : object 'ds' not found
@@ -115,11 +116,12 @@ act=active%>%
 
 
 #------------------------------------------------------------------------
-Age <- ds_some_rows_v1%>%
-    filter(!is.na(analysis_mat_age))
+Age <- act%>%
+    filter(!is.na(beachphone_age))%>%
+    distinct(test_id, .keep_all = TRUE)
 
-BMI <- ds_some_rows_v1%>%
-    filter(!is.na(analysis_bmi))
+BMI <- act%>%
+    filter(!is.na(mom3t_prepreg_bmi))
 
 #------------------------------------------------------------------------
 
@@ -136,22 +138,21 @@ ui <- fluidPage(
     
     sidebarLayout(
         sidebarPanel(
-            radioButtons("Rtype","Recruitment type",choices = c("Facebook","Flyer","Other")),
             sliderInput("quarter","Quarter",value = c(1,2),min=1,max=4),
             sliderInput("year","Years",value = c(2017,2018),min=2017,max=2019),
-            checkboxGroupInput("encounter","Encounter Type",choices = c("Facebook","Flyer","Other"),selected ="Facebook"),
-            sliderInput("BMI","BMI range",value = c(25,35),min=min(BMI$analysis_bmi),max=max(BMI$analysis_bmi)),
-            sliderInput("Age","Age range",value = c(30,40),min=min(Age$analysis_mat_age),max=max(Age$analysis_mat_age))
+            checkboxGroupInput("encounter","Encounter Type",choices = c("Social Media","Flyer","Radio", "Newspaper","word-of-mouth","other"),selected ="Social Media"),
+            sliderInput("BMI","BMI range",value = c(25,35),min=min(BMI$mom3t_prepreg_bmi),max=max(BMI$mom3t_prepreg_bmi)),
+            sliderInput("Age","Age range",value = c(30,40),min=min(Age$beachphone_age),max=max(Age$beachphone_age))
         ),
         
         
         # Show a plot of the generated distribution
         mainPanel(
             tabsetPanel(
-                tabPanel("Recruitment By Time",plotOutput("distPlot")),
+                tabPanel("Recruitment By Time",plotOutput("distPlot"), tableOutput("recruitmentType")),
                 tabPanel("Encounter Recruitment",plotOutput("encounterRecruitment")),
-                tabPanel("BMI by Recruitment",plotOutput("BMIPlot"),tableOutput("BMITable")),
-                tabPanel("Age by recruitment",plotOutput("AgePlot"),tableOutput("AgeTable"))
+                tabPanel("BMI by Recruitment",plotOutput("BMIPlot")),
+                tabPanel("Age by recruitment",plotOutput("AgePlot"))
             )
         )
     )
@@ -168,25 +169,25 @@ server <- function(input, output) {
     
     output$distPlot <- renderPlot({
         
-        Encounter_per_time <- ds_some_rows_v1%>%
-            select(encounter_date_int,learn_about_study)
+        Encounter_per_time <- act%>%
+            select(Encounter_Date,Encounter_Type)
         
-        Encounter_per_time$encounter_date_int <- as.Date(Encounter_per_time$encounter_date_int, format= "%Y-%m-%d")
+        Encounter_per_time$Encounter_Date <- as.Date(Encounter_per_time$Encounter_Date, format= "%Y-%m-%d")
         
-        my_data <- data.frame(encounter_date_int = seq(as.Date("2017-01-01"), as.Date("2019-12-31"), 
+        my_data <- data.frame(Encounter_Date = seq(as.Date("2017-01-01"), as.Date("2019-12-31"), 
                                                        by = "1 month"), format= "%Y-%m-%d")
         
-        my_complete <- full_join(my_data, Encounter_per_time, by = "encounter_date_int")
+        my_complete <- full_join(my_data, Encounter_per_time, by = "Encounter_Date")
         
         Encounter_per_time <- my_complete%>%
-            separate(encounter_date_int,c("y","m","d"))%>%
+            separate(Encounter_Date,c("y","m","d"))%>%
             mutate(Q = case_when(
                 m == "01" | m == "02" | m == "03" ~ "1",
                 m == "04" | m == "05" | m == "06" ~ "2",
                 m == "07" | m == "08" | m == "09" ~ "3",
                 m == "10" | m == "11" | m == "12" ~ "4",
                 TRUE~"5"))%>%
-            group_by(learn_about_study,y,Q)%>%
+            group_by(Encounter_Type,y,Q)%>%
             #does not work properly due to slider providing min and max values rather than all values within range
             filter(y %in% (input$year[1]:input$year[2]) & Q %in% (input$quarter[1]:input$quarter[2]))%>%
             summarize(count=n())%>%
@@ -196,7 +197,7 @@ server <- function(input, output) {
         theme_set(theme_classic())
         # distribution of visits each month(histogram)
         h <- ggplot(Encounter_per_time, aes(YQ, count)) + scale_fill_brewer(palette = "Spectral")
-        h + geom_histogram(aes(fill=factor(learn_about_study)), stat = "Identity",
+        h + geom_histogram(aes(fill=factor(Encounter_Type)), stat = "Identity",
                            bins=24,
                            col="black", 
                            size=.1) + # change number of bins
@@ -210,27 +211,40 @@ server <- function(input, output) {
             theme(axis.text.x = element_text(angle=70, vjust =.6))
     })
     
+    output$recruitmentType <- renderTable({
+        recruit <- act%>%
+            select(test_id, Encounter_Type, beach_part_drop_out)%>%
+            distinct(test_id, .keep_all = TRUE)%>%
+            group_by(Encounter_Type, beach_part_drop_out)%>%
+            summarize(count=n())%>%
+            rename("Encoutner method"=Encounter_Type,"Drop participant count"=beach_part_drop_out)
+        
+        formattable(recruit, align = c("l","c","r"))
+    })
+    
     output$encounterRecruitment <- renderPlot({
-        Encounter_per_time <- ds_some_rows_v1%>%
-            select(encounter_date_int,learn_about_study)
+#------------------works for BEACH data----------------------        
         
-        Encounter_per_time$encounter_date_int <- as.Date(Encounter_per_time$encounter_date_int, format= "%Y-%m-%d")
-        
-        my_data <- data.frame(encounter_date_int = seq(as.Date("2017-01-01"), as.Date("2019-12-31"), 
+        Encounter_per_time <- act%>%
+            select(Encounter_Date,Encounter_Type)%>%
+            distinct(test_id, .keep_all = TRUE)
+#Should compare months and only add those that are not within the already made set.
+#this can be done by using %in% with the Dates. must learn how to select and compare months.        
+        my_data <- data.frame(Encounter_Date = seq(as.Date("2017-01-01"), as.Date("2019-12-31"), 
                                                        by = "1 month"), format= "%Y-%m-%d")
         
-        my_complete <- full_join(my_data, Encounter_per_time, by = "encounter_date_int")
+        my_complete <- full_join(my_data, Encounter_per_time, by = "Encounter_Date")
         
         Encounter_per_time <- my_complete%>%
-            separate(encounter_date_int,c("y","m","d"))%>%
+            separate(Encounter_Date,c("y","m","d"))%>%
             mutate(Quarter = case_when(
                 m == "01" | m == "02" | m == "03" ~ "Q1",
                 m == "04" | m == "05" | m == "06" ~ "Q2",
                 m == "07" | m == "08" | m == "09" ~ "Q3",
                 m == "10" | m == "11" | m == "12" ~ "Q4",
                 TRUE~"Q5"))%>%
-            group_by(learn_about_study,y,Quarter)%>%
-            filter(learn_about_study %in% input$encounter)%>%
+            group_by(Encounter_Type,y,Quarter)%>%
+            filter(Encounter_Type %in% input$encounter)%>%
             summarize(count=n())%>%
             unite("YQ",c("y",Quarter),sep ="-")
         
@@ -238,7 +252,7 @@ server <- function(input, output) {
         theme_set(theme_classic())
         # distribution of visits each month(histogram)
         h <- ggplot(Encounter_per_time, aes(YQ, count)) + scale_fill_brewer(palette = "Spectral")
-        h + geom_histogram(aes(fill=factor(learn_about_study)), stat = "Identity",
+        h + geom_histogram(aes(fill=factor(Encounter_Type)), stat = "Identity",
                            bins=24,
                            col="black", 
                            size=.1) + # change number of bins
@@ -253,80 +267,67 @@ server <- function(input, output) {
     })
     
     output$BMIPlot <- renderPlot({
-        BMI <- ds_some_rows_v1%>%
-            select(record_id,analysis_bmi,learn_about_study)%>%
-            filter(!is.na(analysis_bmi))%>%
-            filter(!is.na(learn_about_study))
+#==================Works with BEACH DATA=====================
+        
+        
+        BMI <- act%>%
+            select(test_id,mom3t_prepreg_bmi,Encounter_Type)%>%
+            filter(!is.na(mom3t_prepreg_bmi))%>%
+            filter(!is.na(Encounter_Type))
         
         
         
         #create a marginal Histogram / Boxplot for maternal ages by encounter type:
-        BMI$learn_about_study <- as.factor(BMI$learn_about_study)
+        BMI$Encounter_Type <- as.factor(BMI$Encounter_Type)
         
         library(ggplot2)
         theme_set(theme_classic())
         
         # Plot
-        means <- aggregate(analysis_bmi ~  learn_about_study, BMI, mean)
+        means <- aggregate(mom3t_prepreg_bmi ~  Encounter_Type, BMI, mean)
         
         
-        g <- ggplot(BMI, aes(learn_about_study, analysis_bmi))
+        g <- ggplot(BMI, aes(Encounter_Type, mom3t_prepreg_bmi))
         g + geom_boxplot(varwidth=T, fill="plum") + 
             labs(title="BMI of participants", 
                  subtitle="Divided into how they heard of the study",
                  caption="Source: BEACH Interview",
                  x="How did you hear of our study",
                  y="BMI")+
-            geom_text(data = means, aes(label = analysis_bmi, y = analysis_bmi + 0.08))
-    })
-    
-    output$BMITable <- renderTable({
-        BMI <- ds_some_rows_v1%>%
-            select(analysis_bmi,record_id,learn_about_study)%>%
-            filter(between(analysis_bmi,input$BMI[1],input$BMI[2]))%>%
-            group_by(learn_about_study)%>%
-            summarise(count=n())%>%
-            rename("encounter method"=learn_about_study,"Total"=count)
-        formattable(BMI,align=c("l","r"))
-        
+            geom_text(data = means, aes(label = mom3t_prepreg_bmi, y = mom3t_prepreg_bmi + 0.08))
     })
     
     output$AgePlot <- renderPlot({
-        Mat_age <- ds_some_rows_v1%>%
-            select(record_id,analysis_mat_age,learn_about_study)%>%
-            filter(!is.na(analysis_mat_age))%>%
-            filter(!is.na(learn_about_study))
+#=====================Works with BEACH Data======================        
+        
+        
+        Mat_age <- act%>%
+            select(test_id,beachphone_age,Encounter_Type)%>%
+            filter(!is.na(beachphone_age))%>%
+            filter(!is.na(Encounter_Type))
         
         
         #create a marginal Histogram / Boxplot for maternal ages by encounter type:
-        Mat_age$learn_about_study <- as.factor(Mat_age$learn_about_study)
+        Mat_age$Encounter_Type <- as.factor(Mat_age$Encounter_Type)
         
         library(ggplot2)
         theme_set(theme_classic())
         
         # Plot
-        means <- aggregate(analysis_mat_age ~  learn_about_study, Mat_age, mean)
+        means <- aggregate(beachphone_age ~  Encounter_Type, Mat_age, mean)
         
-        g <- ggplot(Mat_age, aes(learn_about_study, analysis_mat_age))
+        g <- ggplot(Mat_age, aes(Encounter_Type, beachphone_age))
         g + geom_boxplot(varwidth=T, fill="plum") + 
             labs(title="Age of Participants", 
                  subtitle="Divided into how they heard of the study",
                  caption="Source: BEACH Interview",
                  x="How did you hear of our study",
                  y="Age(years)")+
-            geom_text(data = means, aes(label = analysis_mat_age, y = analysis_mat_age + 6))
+            geom_text(data = means, aes(label = beachphone_age, y = beachphone_age + 6))
     })
     
     #change to DT table rather than formatabe 
-    output$AgeTable <- renderTable({
-        Mat_age <- ds_some_rows_v1%>%
-            select(record_id,analysis_mat_age,learn_about_study)%>%
-            filter(between(analysis_mat_age,input$Age[1],input$Age[2]))%>%
-            filter(!is.na(analysis_mat_age))%>%
-            filter(!is.na(learn_about_study))
-        
-        formattable(Mat_age,asign=c("l","c","r"))
-    })
+    
     
     
 }
